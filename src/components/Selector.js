54e02568a1b2a77138612019555d522cc4856e08
Nao Nakagawa <nao_nkgw@icloud.com>
2024-06-28 10:29:19 +0200

    changed Selector.js

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -1,8 +1,16 @@
-import React from "react";
-// import countriesJson from "../countries.json";
+// Selector.js
+
+import countriesJson from "../countries.json";
 
 const Selector = () => {
-  return <div>console.log(countriesJson);</div>;
+  console.log(countriesJson); // 追加
+  return (
+    <div>
+      <select>
+        {/* countriesJson.map((x)=> {<option>x.slug</option>})  */}
+      </select>
+    </div>
+  );
 };
 
 export default Selector;
